7aeafc6cb529fa6d12c7b922851e3f8228aebdb6
TRI22TGR <tegarnrp@iet.student.pens.ac.id>
2023-05-19 09:43:43 +0700

    pras2

--- lib/main.dart
+++ lib/main.dart
@@ -40,7 +40,7 @@ class AppHome extends StatelessWidget {
             ),
             OutlinedButton(
               onPressed: () {},
-              child: const Text("Outlined Button"),
+              child: const Text("Outlined Butt"),
             ),
             const Padding(
               padding: EdgeInsets.all(20.0),
